55700b6da023685f91281d11f083da407ad2a86a
Nikhil Narang <nikhilnarang@Nikhils-MacBook.local>
2017-01-09 19:58:38 -0800

    added sprinkio iphone app files

---
+++ Sprinkler Controller/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Sprinkler Controller
+//
+//  Created by Nikhil Narang on 4/8/16.
+//  Copyright © 2016 Nikhil Narang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
